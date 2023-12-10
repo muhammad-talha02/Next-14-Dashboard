26e12aa4a9105ffd8d0fe4b4778abd5c4ecf651a
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-12-10 11:04:33 +0000

    Again

--- app/authConfig.ts
+++ app/authConfig.ts
@@ -1,5 +1,3 @@
-import { NextResponse } from "next/server";
-
 export const authConfig = {
     providers:[],
     pages:{
@@ -15,7 +13,7 @@ export const authConfig = {
             }
             else if(isLoggedIn){
                 console.log("Loggiinnng")
-                return NextResponse.redirect(new URL("/dashboard", request.url))
+                return Response.redirect(new URL("/dashboard", request.url))
             }
             return true
         }
